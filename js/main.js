5adf4823e9c57da6ad39af33beea9af278624641
Fran Martinez <pmg.webdev@gmail.com>
2023-10-30 16:40:10 +0100

    character stays on top of objects while they move and its also pushed

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ class Player {
     // initialize properties
     this.width = 2;
     this.height = 4;
-    this.positionX = 3;
-    this.positionY = 0;
+    this.positionX = 1;
+    this.positionY = 24;
     this.player = null;
 
-    this.gravityOn = false
+    this.gravityOn = true;
+    this.onGround = false;
 
     //dom manipulation
     this.player = document.getElementById('player');
@@ -20,35 +21,61 @@ class Player {
     
     this.move();
     this.gravity();
+    
   }
   move(){      
       this.player.style.left = this.positionX + "em";
-      this.player.style.left = this.positionX + "em";
       this.player.style.bottom = this.positionY + "em";
-      this.player.style.bottom = this.positionY + "em";
+      
   }
 jump(){
-  for(let i=0; i < 5; i++){
+  for(let i=0; i < 10; i++){
     this.positionY++;
   };
   this.player.style.bottom = this.positionY + "em";
+  this.onGround = false;
 };
 gravity() {
-  setInterval(() => {
-    this.gravityOn = true
-    if (this.positionY > 0 && this.gravityOn) {
-      this.positionY--;
-      this.player.style.bottom = this.positionY + "em";
-    } 
-    obstaclesArr.forEach(obs => {
-     if(collisionCheck(obs)){
-      this.gravityOn = false
-     }
-    })
-  }, 100)
-};  
+  if(this.onGround) {
+    this.gravityOn = false
+    this.onGround = true
+  } else if (this.onGround === false && this.positionY > 0) {
+    this.gravityOn = true;
+    this.onGround = false;
+    this.positionY -=  0.3;
+     this.move();
+  } else if(this.positionY === 0){
+    this.onGround = true;
+    this.gravityOn = false;
+  }
+    // this.gravityOn = true
+    // obstaclesArr.forEach(obs => {
+    //  if(collisionCheck(obs)){
+    //   this.gravityOn = false
+    //   console.log(this.onGround)
+    //  }
+    // });
+
+    // if (this.positionY > 0 && this.gravityOn && !this.onGround) {
+    //   this.positionY--;
+    //   this.move()
+    //   console.log(this.onGround)
+    // } 
+    // if(this.positionY === 0){
+    //   this.onGround = true;
+    //   console.log(this.onGround)
+    // } else if(this.gravityOn === false){
+    //   this.onGround = false;
+    //   console.log(this.onGround)
+    // } 
+}
 }
 
+// setInterval(() => {
+//   if(player.gravityOn) {
+//     move()
+//   }
+// },10)
 
 //////////// OBSTACLE
 
@@ -81,16 +108,57 @@ class Obstacle {
   }
 }
 
+class Platform {
+  constructor(){
+    this.width = 4;
+    this.height = 1;
+    this.positionY = 23;
+    this.positionX = 0;
+    this.platform = null;
 
+    this.platform = document.getElementById('platform');
+    this.platform.style.width = this.width + 'vw';
+    this.platform.style.height = this.height + 'vw';
+
+    this.ground();
+  }
+  ground(){
+    if(platformCollision(player, this.platform)){
+      console.log('ground')
+    }
+  }
+  
+}
 
 
 /////////////// GAME INITIALIZE   ///////////////////////////
 const player = new Player();
 const obstaclesArr = [];
+const platform = new Platform(); 
+
+
 
 
 
 ///////////// SETTINGS/////////////////////////////////
+
+///////////////////////// Game loop
+function gameLoop() {
+  player.move();
+  player.gravity();
+
+  
+  //////////////////////   On the platform
+  platformCollision(player, platform);
+
+  // Stays on TOP!!
+  obstaclesArr.forEach((obstacle) => {  
+  onTop(player, obstacle) 
+})
+
+
+  requestAnimationFrame(gameLoop);    // This keeps the loop running
+}
 
 
 ///////////////////////////////////////////////////  Player MOVEMENT
@@ -117,7 +185,6 @@ document.addEventListener("keydown", (e) => {
       break;
   }
 });
-
 ///////////////////////////////////////////// Obstacles CREATION
 setInterval(() => {
   const newObstacle = new Obstacle();
@@ -127,14 +194,13 @@ setInterval(() => {
 ////////////////////////////////////////////// Obstacle MOVEMENT and REMOVE obstacle
 setInterval(() => {
   obstaclesArr.forEach((obstacle) => {
-    if (!collisionCheck(obstacle)) {
-      obstacle.moveLeft();
-    }
+    obstacle.moveLeft();
     if(obstacle.positionX + obstacle.width < 0){
       obstaclesArr.splice(obstacle[0], 1);
     }
   });
 }, 200);
+
 
 /////////////////////////////////////////////////////// Obstacle mov and PUSH player
 setInterval(() => {
@@ -145,26 +211,46 @@ setInterval(() => {
     }
   });
 }, 200);
-
-
 ////////////////////////// Detect COLLISION 
-
-function collisionCheck(obstacle) {
+function platformCollision(r1, r2){   
   if (
-    player.positionX < obstacle.positionX + obstacle.width &&
-    player.positionX + player.width > obstacle.positionX &&
-    player.positionY < obstacle.positionY + obstacle.height &&
-    player.positionY + player.height > obstacle.positionY
-  ) {
-    if(player.positionY > obstacle.positionY ){
-      player.positionY =  obstacle.height 
-      player.positionX-- 
- player.move()
-      player.gravityOn = false
+    r1.positionX < r2.positionX + r2.width&&
+    r1.positionX + r1.width > r2.positionX &&
+    r1.positionY < r2.positionY + r2.height &&
+    r1.positionY + r1.height > r2.positionY
+    ){
+      player.onGround = true;
+    } else{
+      player.onGround = false;
     }
-    console.log('collision');
-    return true;
-  } else {
-    return false;
   }
-};
+
+platformCollision(player, platform)
+
+function onTop(r1, r2) {
+  if (
+    r1.positionX < r2.positionX + r2.width&&
+    r1.positionX + r1.width > r2.positionX &&
+    r1.positionY < r2.positionY + r2.height &&
+    r1.positionY + r1.height > r2.positionY
+    ){
+      player.onGround = true;
+      player.gravityOn = false;
+    };
+  };
+
+  function collisionCheck(obstacle) {
+    if (
+      player.positionX < obstacle.positionX + obstacle.width &&
+      player.positionX + player.width > obstacle.positionX &&
+      player.positionY < obstacle.positionY + obstacle.height &&
+      player.positionY + player.height > obstacle.positionY
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+
+gameLoop();
